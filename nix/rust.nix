c8a9785414322149f373fec2a9f5a1e7467fcac7
foldu <foldu@protonmail.com>
2020-02-23 12:39:39 +0100

    nixfmt

--- nix/rust.nix
+++ nix/rust.nix
@@ -4,7 +4,7 @@ let
   pkgs = import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
   date = "2020-02-14";
 in
-  pkgs.rustChannelOf { 
-    inherit date;
-    channel = "nightly";
-  }
+pkgs.rustChannelOf {
+  inherit date;
+  channel = "nightly";
+}
